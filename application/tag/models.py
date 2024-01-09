21da7c091d253b0966c3f92c03471d26cfbac656
delav <delavpeng@gmail.com>
2024-01-09 10:44:39 +0800

    opt tag model

--- application/tag/models.py
+++ application/tag/models.py
@@ -7,10 +7,20 @@ class Tag(models.Model):
     id = models.BigAutoField(primary_key=True, help_text='primary key id')
     name = models.CharField(max_length=255, help_text='tag name')
     project_id = models.IntegerField(help_text='associated project')
-    module_id = models.IntegerField(help_text='associated module id')
-    module_type = models.IntegerField(default=0, help_text='associated module type')
 
     class Meta:
         verbose_name = 'tag'
         verbose_name_plural = verbose_name
         db_table = 'tag'
+
+
+class ModuleTag(models.Model):
+    id = models.BigAutoField(primary_key=True, help_text='primary key id')
+    tag_id = models.IntegerField(help_text='associated tag id')
+    module_id = models.IntegerField(help_text='associated module id')
+    module_type = models.IntegerField(default=0, help_text='associated module type')
+
+    class Meta:
+        verbose_name = 'module tag'
+        verbose_name_plural = verbose_name
+        db_table = 'module_tag'
